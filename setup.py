06ae9b8d371287c4f24a9827d0aeeabecbbf3f5c
Paweł Mandera <pawel@pawelmandera.com>
2017-09-05 21:37:40 +0200

    Add numpy to include_dirs in setup.py
    This fixes the problem with compilation on some machines.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
+import numpy
+
 setup(
-    ext_modules = cythonize("sandpile_inner.pyx")
+    ext_modules = cythonize("sandpile_inner.pyx"),
+    include_dirs=[numpy.get_include()]
 )
